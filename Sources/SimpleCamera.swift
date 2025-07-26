2a9e3d793078dfda9913a61e847fb5b5ec0328d8
p-x9 <50244599+p-x9@users.noreply.github.com>
2025-07-27 00:16:28 +0900

    Stop using `TARGET_OS_SIMULATOR` flag

--- Sources/SimpleCamera.swift
+++ Sources/SimpleCamera.swift
@@ -755,7 +755,8 @@ public final class SimpleCamera: NSObject, SimpleCameraInterface {
     private var isConfigured: Bool = false
 
     private func configure() { // この configure 実行でカメラの許可ダイアログが出る
-        if isConfigured || (TARGET_OS_SIMULATOR != 0) {
+#if !targetEnvironment(simulator)
+        if isConfigured {
             return
         }
         defer {
@@ -899,10 +900,12 @@ public final class SimpleCamera: NSObject, SimpleCameraInterface {
         sessionQueue.sync {
             addObservers()
         }
+#endif
     }
 
     private func tearDown() {
-        if !isConfigured || (TARGET_OS_SIMULATOR != 0) {
+#if !targetEnvironment(simulator)
+        if !isConfigured {
             return
         }
 
@@ -928,6 +931,7 @@ public final class SimpleCamera: NSObject, SimpleCameraInterface {
             audioDeviceInput = nil
             isConfigured = false
         }
+#endif
     }
 
     // MARK: KVO and Notifications
